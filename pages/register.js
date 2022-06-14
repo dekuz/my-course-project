1026a29f11f77cfe3aa3ae52ec6dd479f729bdd7
dekuz <kusyanadede@gmail.com>
2022-06-14 10:44:50 +0700

    submit button

--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,54 @@
+import { useState } from "react";
+
+
 const Register = ()=>{
+
+    const [name,setName]=useState('')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.table({name,email,password})
+    };
     return(
         <>
             <h1 className="p-5 big-header text-center bg-primary square">Register </h1>
             
+            <div className="container col-md-4 offset-md-4 pb-5">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text" 
+                        className="form-control mb-4 p-1" 
+                        value={name} 
+                        onChange={e=>setName(e.target.value)}
+                        placeholder="Enter name" 
+                        required
+                    />
+
+                    <input
+                        type="email" 
+                        className="form-control mb-4 p-1" 
+                        value={email} 
+                        onChange={e=>setEmail(e.target.value)}
+                        placeholder="Enter email" 
+                        required
+                    />
+
+                    <input
+                        type="password" 
+                        className="form-control mb-4 p-1" 
+                        value={password} 
+                        onChange={e=>setPassword(e.target.value)}
+                        placeholder="Enter password" 
+                        required
+                    />
+                    
+                    <button type="submit" className="btn btn-block btn-primary">
+                        Submit
+                    </button>
+                </form>
+            </div>
         </>
     )
 }
